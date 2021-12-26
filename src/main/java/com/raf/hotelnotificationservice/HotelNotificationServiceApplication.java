168233a0acd5486c5dd6d17a1e352ee91424e430
SrdjanLazic <slazic3219rn@raf.rs>
2021-12-26 22:27:03 +0100

    Initial commit

---
+++ src/main/java/com/raf/hotelnotificationservice/HotelNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raf.hotelnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raf/hotelnotificationservice/HotelNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.hotelnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
